a26502c046f799a69f567f8ad3341678e78c60c6
Joseph Davis <joethebartenderpdx@gmail.com>
2021-08-05 09:27:28 -0700

    getCharacters function takes in page number as arg

--- src/services/getCharacters.js
+++ src/services/getCharacters.js
@@ -1,6 +1,6 @@
 /* eslint-disable max-len */
-export async function getCharacters() {
-  const res = await fetch('https://hey-arnold-api.herokuapp.com/api/v1/characters');
+export async function getCharacters(page) {
+  const res = await fetch(`https://hey-arnold-api.herokuapp.com/api/v1/characters?page=${page}`);
   const json = await res.json();
   return json;
 }

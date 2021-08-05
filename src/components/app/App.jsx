ced2f76fdcbb9e5fb5cb9f288c552c0a6b106249
Joseph Davis <joethebartenderpdx@gmail.com>
2021-08-04 17:26:28 -0700

    minimalistic button

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from '../Header';
 import Image from '../Image';
 import List from '../List';
 
 export default function App() {
   return (
     <Router>
+      <Header />
       <Switch>
 
         <Route path="/" exact={true}>
@@ -15,7 +17,7 @@ export default function App() {
         <Route path="/:id" exact={true}>
           <Image />
         </Route>
-        
+
       </Switch>
     </Router>
   );
